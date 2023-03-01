e3071c128c717451c68bb2a6272e8d1bb9459bf1
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2023-03-01 15:02:48 -0300

    rand2

--- common/c_randdp.go
+++ common/c_randdp.go
@@ -1,5 +1,7 @@
 package common
 
+ 
+
 func randlc(*x, a float64){
 	var t1,t2,t3,t4,a1,a2,x1,x2,z float64
 
@@ -18,4 +20,29 @@ func randlc(*x, a float64){
 	(*x) = t3 - t46 * t4
 
 	return (r46 * (*x))
+}
+
+func vrandlc(n int, *x_speed float64, float64 a, y []float64){
+	var i int
+	var x,t1,t2,t3,t4,a1,a2,x1,x2,z float64
+
+	t1 = r23 * a
+	a1 = int64(t1)
+	a2 = a - t23 * a1
+	x - *x_speed
+
+	for i:=0; i < n; i++{
+		t1 = r23 * x
+		x1 = int64(t1)
+		x2 = x - t23 * x1
+		t1 = a1 * x2 + a2 * x1
+		t2 = int64(r23 * t1)
+		z = t1 - t23 * t2
+		t3 = t23 * z + a2 * x2
+		t4 = int64(r46 * t3)
+		x = t3 - t46 * t4
+		y[i] = r46 * x
+	}
+
+	*x_speed = x
 }
