458409280f3555abb104c340e0a4363b2513276f
Raíssa <rncoelho@inf.ufpel.edu.br>
2023-03-03 14:28:54 -0300

    letra faltando

--- common/c_print_results.go
+++ common/c_print_results.go
@@ -2,11 +2,11 @@ package common
 
 import (
 	"fmt"
-	"time"
 	"runtime"
+	"time"
 )
 
-func C_print_results(class string, opType string, niter int, passed_verification bool, Mops float64, t *time.Duration, totalThreads string){
+func C_print_results(class string, opType string, niter int, passed_verification bool, Mops float64, t *time.Duration, totalThreads string) {
 	fmt.Printf("Benchmark Completed!\n")
 	fmt.Printf("Class NPB =%v\n", class)
 	fmt.Printf("Total threads =%v\n", totalThreads)
@@ -14,16 +14,16 @@ func C_print_results(class string, opType string, niter int, passed_verification
 	fmt.Printf("Time in seconds =%v\n", *t)
 	fmt.Printf("Mops total =%v\n", Mops)
 	fmt.Printf("Operation type =%v\n", opType)
-	
-	if(passed_verification){
+
+	if passed_verification {
 		fmt.Println("Verification = SUCCESSFUL")
-	}else{
+	} else {
 		fmt.Println("Verification = UNSUCCESSFUL")
 	}
-	
+
 	fmt.Println("Compiler Version =", runtime.Version())
 
-	/* 
+	/*
 	 * fmt.Printf(" Please send the results of this run to:\n\n");
 	 * fmt.Printf(" NPB Development Team\n");
 	 * fmt.Printf(" Internet: npb@nas.nasa.gov\n \n");
@@ -33,14 +33,14 @@ func C_print_results(class string, opType string, niter int, passed_verification
 	 * fmt.Printf(" Moffett Field, CA  94035-1000\n\n");
 	 * fmt.Printf(" Fax: 650-604-3957\n\n");
 	 */
-	
-	fmt.Printf("\n\n");
-	
-	fmt.Printf("----------------------------------------------------------------------\n");
-	fmt.Printf("NPB-GO by: \n");
-	fmt.Printf("Bianca Nunes Coelho\n");
-	fmt.Printf("Raíssa Nnes Coelho\n");
-	fmt.Printf("----------------------------------------------------------------------\n");
-	fmt.Printf("\n");
-	
+
+	fmt.Printf("\n\n")
+
+	fmt.Printf("----------------------------------------------------------------------\n")
+	fmt.Printf("NPB-GO by: \n")
+	fmt.Printf("Bianca Nunes Coelho\n")
+	fmt.Printf("Raíssa Nunes Coelho\n")
+	fmt.Printf("----------------------------------------------------------------------\n")
+	fmt.Printf("\n")
+
 }
