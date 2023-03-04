49a58514e29f81b45832663499b9611b1fbb52bd
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2023-03-04 14:05:39 -0300

    Add files via upload

---
+++ common/c_file.go
@@ -0,0 +1,67 @@
+package common
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+func C_file(bench string, M int, gc, sx, sy float64, NQ int, q []float64, rng string, nit int, verified bool, Mops float64, t *time.Duration) {
+	var aux string
+	if runtime.GOOS == "windows" {
+		aux = "..\\bin\\"
+	} else {
+		aux = "../bin/"
+	}
+
+	var n string
+	if M == 24 {
+		n = "S"
+	} else if M == 25 {
+		n = "W"
+	} else if M == 28 {
+		n = "A"
+	} else if M == 30 {
+		n = "B"
+	} else if M == 32 {
+		n = "C"
+	} else if M == 36 {
+		n = "D"
+	} else if M == 40 {
+		n = "E"
+	}
+
+	f, err := os.Create(aux + bench + "_" + n + ".txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.WriteString("Benchmark :" + bench + "\n")
+	f.WriteString("N = " + strconv.Itoa(M) + "\n")
+	f.WriteString("No. Gaussian Pairs = " + fmt.Sprint(gc) + "\n")
+	f.WriteString("Sums = " + fmt.Sprint(sx) + "  " + fmt.Sprint(sy) + "\n")
+	f.WriteString("Counts: \n")
+	for i := 0; i < NQ-1; i++ {
+		f.WriteString(strconv.Itoa(i) + " - " + fmt.Sprint(q[i]) + "\n")
+	}
+	f.WriteString("Benchmark Completed!\n")
+	f.WriteString("Class NPB = " + n + "\n")
+	f.WriteString("Total threads = " + strconv.Itoa(runtime.NumCPU()) + "\n")
+	f.WriteString("Iterations = " + strconv.Itoa(nit) + "\n")
+	f.WriteString("Time = " + fmt.Sprint(*t) + "\n")
+	f.WriteString("Mop/s total = " + fmt.Sprint(Mops) + "\n")
+	f.WriteString("Operation type = " + rng + "\n")
+
+	if verified {
+		f.WriteString("Verification = SUCCESSFUL\n")
+	} else {
+		f.WriteString("Verification = UNSUCCESSFUL\n")
+	}
+
+	f.WriteString("Compiler Version = " + fmt.Sprint(runtime.Version()))
+
+	defer f.Close()
+}
