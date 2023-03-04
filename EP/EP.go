94e67761c7fc087032cecc535412e881e2dde7fa
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2023-03-04 14:07:58 -0300

    Update EP.go

--- EP/EP.go
+++ EP/EP.go
@@ -182,28 +182,36 @@ func Ep(M int){
 	//Verification of the values.
 	nit = 0
 	verified = true
+	var n string
 	
 	if M == 24 {
 		sx_verify_value = -3.247834652034740e+3
 		sy_verify_value = -6.958407078382297e+3
+		n = "S"
 	}else if  M == 25 {
 		sx_verify_value = -2.863319731645753e+3
 		sy_verify_value = -6.320053679109499e+3
+		n = "W"
 	}else if M == 28 {
 		sx_verify_value = -4.295875165629892e+3
 		sy_verify_value = -1.580732573678431e+4
+		n = "A"
 	}else if M == 30 {
 		sx_verify_value =  4.033815542441498e+4
 		sy_verify_value = -2.660669192809235e+4
+		n = "B"
 	}else if M == 32 {
 		sx_verify_value =  4.764367927995374e+4
 		sy_verify_value = -8.084072988043731e+4
+		n = "C"
 	}else if M == 36 {
 		sx_verify_value =  1.982481200946593e+5
 		sy_verify_value = -1.020596636361769e+5
+		n = "D"
 	}else if M == 40 {
 		sx_verify_value = -5.319717441530e+05
 		sy_verify_value = -3.688834557731e+05
+		n = "E"
 	}else {
 		verified = false
 	}
@@ -227,7 +235,7 @@ func Ep(M int){
 	 }
 	
 	
-	
-	r.C_print_results( " EP"," Random Numbers Generated",nit,verified,Mops,&t,runtime.NumCPU())
+	r.C_file("EP", M, gc, sx, sy, NQ, q[:], " Random Numbers Generated", nit, verified, Mops, &t)
+	r.C_print_results(n," Random Numbers Generated",nit,verified,Mops,&t,runtime.NumCPU())
 		 
 }
